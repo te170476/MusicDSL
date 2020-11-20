4e3bc45df2d9d1bb74d3d293067c43ebfaa5cbc0
te170476 <te170476@gmail.com>
2020-11-20 12:08:38 +0900

    :recycle: Sin波生成コードの改善

--- src/main/java/com/github/te170476/musicdsl/Generator.java
+++ src/main/java/com/github/te170476/musicdsl/Generator.java
@@ -1,16 +1,18 @@
 package com.github.te170476.musicdsl;
 
 public class Generator {
+    int amplitude = 127;
     int sampleRate;
     public Generator(int sampleRate) {
         this.sampleRate = sampleRate;
     }
+
     public byte[] sin(double hertz, double second){
         int length = (int)(sampleRate * second);
         byte[] wave = new byte[length];
-        double pitch = sampleRate / hertz;
+        double byteParAmplitude = sampleRate / hertz;
         for(int index = 0; index < wave.length; index++){
-            wave[index] = (byte) (110 * Math.sin((index/pitch) * Math.PI * 2));
+            wave[index] = (byte) (amplitude * Math.sin((index / byteParAmplitude) * Math.PI * 2.0));
         }
         return wave;
     }
