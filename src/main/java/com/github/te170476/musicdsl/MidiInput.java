9bae1a875bc61950c27d85af2725ebbae7faf11d
te170476 <te170476@gmail.com>
2020-12-01 17:27:52 +0900

    Midi入力のリアルタイム再生処理

--- src/main/java/com/github/te170476/musicdsl/MidiInput.java
+++ src/main/java/com/github/te170476/musicdsl/MidiInput.java
@@ -1,61 +1,124 @@
 package com.github.te170476.musicdsl;
 
-import javax.sound.sampled.*;
-import java.lang.Math;
-import java.io.*;
+import com.github.te170476.musicdsl.sound.Waves;
+import com.github.te170476.musicdsl.sound.generator.WaveGenerator;
 
-import java.util.ArrayList;
-import javax.sound.midi.*;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.concurrent.CopyOnWriteArrayList;
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Transmitter;
+import javax.sound.sampled.*;
+import java.util.*;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.stream.Collectors;
 
 public class MidiInput{
-    static final int SAMPLE_RATE=44100;
-    static byte[] data = new byte[4];
-//-----------------------------------------------
-    public static void main(String[] args) throws MidiUnavailableException {
-        var devices = MidiSystem.getMidiDeviceInfo();
-        for (int i = 0; i < devices.length; i++) {
-            System.out.println(i +": "+ devices[i]);
-        }
+    private final CopyOnWriteArraySet<Integer> onPressMidiKeySet = new CopyOnWriteArraySet<>();
+    private final MidiDevice device;
+    private final AudioFormat format;
 
+    public static Optional<MidiInput> open(MidiDevice device, AudioFormat format) {
+        try {
+            device.open();
+        } catch (MidiUnavailableException e) {
+            return Optional.empty();
+        }
+        return Optional.of(new MidiInput(device, format));
+    }
+    private MidiInput(MidiDevice device, AudioFormat format) {
+        var transmitter = getTransmitter(device).get();
+        transmitter.setReceiver(new MidiInputReceiver(device.toString(), onPressMidiKeySet));
+        this.device = device;
+        this.format = format;
+    }
+
+    public static List<MidiDevice> getMidiDevices() {
+        return Arrays.stream(MidiSystem.getMidiDeviceInfo())
+                .map(MidiInput::getMidiDeviceFrom)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .filter(it-> getTransmitter(it).isPresent())
+                .collect(Collectors.toList());
+    }
+    public static Optional<MidiDevice> getMidiDeviceFrom(MidiDevice.Info info) {
+        try {
+            return Optional.of(MidiSystem.getMidiDevice(info));
+        } catch (MidiUnavailableException e) {
+            return Optional.empty();
+        }
+    }
+    public static Optional<Transmitter> getTransmitter(MidiDevice device) {
+        try {
+            return Optional.of(device.getTransmitter());
+        } catch (MidiUnavailableException e) {
+            return Optional.empty();
+        }
+    }
+
+    private static Optional<SourceDataLine> createSourceDataLine(AudioFormat format) {
+        try {
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+            if (!(AudioSystem.isLineSupported(info)))
+                return Optional.empty();
+            return Optional.of((SourceDataLine) AudioSystem.getLine(info));
+        } catch (LineUnavailableException ignored) {}
+        return Optional.empty();
+    }
+    public void play() {
+        var sampleRate = (int) format.getSampleRate();
+        var sampleSize = format.getSampleSizeInBits();
+        var channelCount = format.getChannels();
+        var bufferSize = sampleSize * channelCount * 64;
+        var generator = new WaveGenerator(sampleRate);
+        var dataLine = createSourceDataLine(format).get();
+        try {
+            dataLine.open(format, bufferSize);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        dataLine.start();
+
+        var offsetMap = new HashMap<Integer, Integer>();
+        while (true) {
+            if(onPressMidiKeySet.size() <= 0) {
+                offsetMap.clear();
+                dataLine.drain();
+                continue;
+            }
+            var waves =
+                    onPressMidiKeySet.stream()
+                            .map(keyCode-> {
+                                if (!offsetMap.containsKey(keyCode))
+                                    offsetMap.put(keyCode, 0);
+                                var pitch = keyCode - 57;
+                                var hertz = Waves.getHertz(pitch);
+                                var offset = offsetMap.get(keyCode);
+                                offsetMap.put(keyCode, offset + bufferSize);
+                                return generator.generate(hertz, bufferSize, offset, Waves.square);
+                            });
+            byte[] wave = new byte[]{};
+            for (byte[] it : waves.collect(Collectors.toList())){
+                wave = Converter.merge(wave, new Sound(it, 0));
+            }
+            dataLine.write(wave, 0, wave.length);
+        }
+    }
+
+    public static void main(String[] args) throws LineUnavailableException {
+        var devices = getMidiDevices();
+        for (int index = 0; index < devices.size(); index++) {
+            System.out.println(index +": "+ devices.get(index).getDeviceInfo());
+        }
         System.out.println("plz select midi device index: ");
         Scanner scan  = new Scanner(System.in);
-        int DEVICE_IN = scan.nextInt();
+        int deviceIndex = scan.nextInt();
+        var device = devices.get(deviceIndex);
 
-        MidiDevice.Info device_info = devices[DEVICE_IN];
-        MidiDevice device_input = MidiSystem.getMidiDevice(device_info);
-
-        var set = new CopyOnWriteArraySet<Integer>();
-        try{
-            Transmitter trans = device_input.getTransmitter();
-
-            trans.setReceiver(new MidiInputReceiver(device_info.toString(), set));
-
-            device_input.open();
-        }catch (MidiUnavailableException e){
-            System.err.println(e.getMessage());
-            System.exit(0);
-        }
-
-//        try{
-//            AudioFormat fmt = new AudioFormat(SAMPLE_RATE, 8 , 1, true, false);
-//            SourceDataLine audio = (SourceDataLine)AudioSystem.getSourceDataLine(fmt);
-//                    audio.open(fmt);
-//                    audio.start();
-//
-//                    while(true){
-//                        for(int i=0; i<4; i++){
-//
-//                        }
-//                        audio.write(data, 0, 4);
-//
-//                    }
-//        }catch (Exception e){
-//            e.printStackTrace();
-//            System.exit(1);
-//        }
+        var sampleRate = 44100;
+        var sampleSize = 8;
+        var channelCount = 1;
+        var format = new AudioFormat(sampleRate, sampleSize, channelCount, true, false);
+        MidiInput.open(device, format)
+            .ifPresent(MidiInput::play);
     }
 }
--- src/main/java/com/github/te170476/musicdsl/MidiInputReceiver.java
+++ src/main/java/com/github/te170476/musicdsl/MidiInputReceiver.java
@@ -10,16 +10,9 @@ import java.util.Set;
 public class MidiInputReceiver implements Receiver {
     public final String name;
     private final Set<Integer> onPressKeySet;
-    private final int sampleRate;
-    private final WaveGenerator generator;
-    private final SourceDataLine dataLine;
     public MidiInputReceiver(String name, Set<Integer> onPressKeySet){
         this.name = name;
         this.onPressKeySet = onPressKeySet;
-        this.sampleRate = 44100;
-        this.generator = new WaveGenerator(sampleRate);
-        AudioFormat format = new AudioFormat(sampleRate, 8, 1, true, false);
-        this.dataLine = createSourceDataLine(format).get();
         System.out.println("midi connect");
     }
 
@@ -42,10 +35,6 @@ public class MidiInputReceiver implements Receiver {
                 break;
         }
         System.out.println("midi received");
-//            List<Sound> sounds = Arrays.asList(
-//                    new Sound(generator.generate(Waves.getHertz(shortMessage.getData1()-57), sampleRate / 4, Waves.sin), 0)
-//            );
-//            player.play(sounds);
     }
     public void onPress(int key) {
         onPressKeySet.add(key);
@@ -56,14 +45,5 @@ public class MidiInputReceiver implements Receiver {
 
     public void close(){}
 
-    private Optional<SourceDataLine> createSourceDataLine(AudioFormat format) {
-        try {
-            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-            if (!(AudioSystem.isLineSupported(info)))
-                return Optional.empty();
-            return Optional.of((SourceDataLine) AudioSystem.getLine(info));
-        } catch (LineUnavailableException ignored) {}
-        return Optional.empty();
-    }
 
 }
