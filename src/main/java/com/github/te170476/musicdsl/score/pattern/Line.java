eca4a6404f1e454bf9a13edcc6d6c98d12e08f72
te170476 <te170476@gmail.com>
2020-12-22 16:55:58 +0900

    delete Line#{toNote(), noteValues()}
    toSoundableじゃないのにこれがあるの、混乱の元なのでとりあえず消した

--- src/main/java/com/github/te170476/musicdsl/score/pattern/Line.java
+++ src/main/java/com/github/te170476/musicdsl/score/pattern/Line.java
@@ -2,11 +2,9 @@ package com.github.te170476.musicdsl.score.pattern;
 
 import com.github.te170476.Streams;
 import com.github.te170476.musicdsl.score.note.AbsoluteNote;
-import com.github.te170476.musicdsl.score.note.INoteValue;
 import com.github.te170476.musicdsl.score.note.NoteValue;
 import com.github.te170476.musicdsl.score.note.RelativeNote;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,16 +12,6 @@ public class Line<T> {
     public final List<RelativeNote<T>> notes;
     public Line(List<RelativeNote<T>> notes) {
         this.notes = notes;
-    }
-
-    public RelativeNote<Line<T>> toNote() {
-        return new RelativeNote<>(noteValue(), this);
-    }
-
-    INoteValue noteValue() {
-        return notes.stream()
-                .map(it-> it.noteValue)
-                .reduce(NoteValue.get(0), (it, sum) -> NoteValue.get(sum, it));
     }
 
     public List<AbsoluteNote<T>> toNotes() {
