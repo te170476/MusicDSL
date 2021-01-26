b44bad76c409c9c6c2e567472c875b40b43b5688
te170476 <te170476@gmail.com>
2021-01-26 17:11:41 +0900

    add: GET rolls/{id}/sound return PCM

--- src/main/scala/com/github/nnnnusui/musicdsl/usecase/Sound.scala
+++ src/main/scala/com/github/nnnnusui/musicdsl/usecase/Sound.scala
@@ -64,37 +64,45 @@ trait Sound {
         val keyA = 9
         val sampleRate = input.sampleRate
         val bar = 4
-        val beat = sampleRate / bar
         val foldedNotes = (folded.store :+ Data(folded.pos, folded.length)).tail
 
         val channel = 1
         val length = sampleRate * roll.division / 4
+        val beat = length / roll.division
         println(foldedNotes)
-        val note = notes.head
-        val hertz = getHertz(note.octave * maxPitch + note.pitch - keyA)
+//        val note = notes.head
+//        val offset = note.offset
+//        val noteLength =
+//          notes
+//            .filter(_.octave == note.octave)
+//            .filter(_.pitch == note.pitch)
+//            .sortBy(_.offset)
+//            .foldLeft(1) { case (sum, it) => if (it.sticky) sum + 1 else sum }
+//        val hertz = getHertz(note.octave * maxPitch + note.pitch - keyA)
+//        val note = foldedNotes.head
+//        val offset = note.pos.offset
+//        val noteLength = note.length
+//        val hertz = getHertz(note.pos.octave * maxPitch + note.pos.pitch - keyA)
+//        val pcm = generate(sampleRate, hertz, offset * beat, noteLength * beat)
+
         val pcm =
-          generate(sampleRate, hertz, 0, length)
-//          Range(0, length)
-//            .map(index => {
-//              sin(sampleRate, index, 440 / 2)
-//            })
-//          foldedNotes
-//            .map(note => {
-//              val offset = beat * note.pos.offset
-//              val length = beat * note.length
-//              val hertz = getHertz(note.pos.octave * maxPitch + note.pos.pitch - keyA)
-//              generate(sampleRate, hertz, offset, length)
-//            })
-//            .fold(Seq.fill(length)(0.0))((sumPcm, pcm) =>
-//              sumPcm
-//                .zip(pcm)
-//                .map {
-//                  case (sum, it) =>
-//                    if (sum == 0.0) it
-//                    else if (it == 0.0) sum
-//                    else sum * 0.5 + it * 0.5
-//                }
-//            )
+          foldedNotes
+            .map(note => {
+              val offset = note.pos.offset
+              val noteLength = note.length
+              val hertz = getHertz(note.pos.octave * maxPitch + note.pos.pitch - keyA)
+              generate(sampleRate, hertz, offset * beat, noteLength * beat)
+            })
+            .fold(Seq.empty[Double])((sumPcm, pcm) =>
+              sumPcm
+                .zipAll(pcm, 0.0, 0.0)
+                .map {
+                  case (sum, it) =>
+                    if (sum == 0.0) it
+                    else if (it == 0.0) sum
+                    else sum * 0.5 + it * 0.5
+                }
+            )
         Output.Get(channel, length, pcm)
       }
   }
