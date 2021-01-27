e147a14185680a0af51fccd5d452c8946a9d5d9f
te170476 <te170476@gmail.com>
2021-01-28 06:32:39 +0900

    add: offset recursive Roll is now soundable

--- src/main/scala/com/github/nnnnusui/musicdsl/usecase/Sound.scala
+++ src/main/scala/com/github/nnnnusui/musicdsl/usecase/Sound.scala
@@ -11,99 +11,118 @@ trait Sound {
   def getRoll(rollId: Int): Future[Option[Roll]]
   def getNotesFromRollId(rollId: Int): Future[Seq[Note]]
 
-  case class Pos(offset: Int, octave: Int, pitch: Int)
-  case class Data(pos: Pos, length: Int)
-  case class Fold(store: Seq[Data], pos: Pos, length: Int, beforeSticky: Boolean)
-  def getHertz(pitch: Int): Double = {
-    val pitchReference = 440.0
-    pitchReference * Math.pow(2, pitch / 12.0)
-  }
-  def square(sampleRate: Int, index: Int, hertz: Double): Double = {
-    val count = 2 * index * hertz / sampleRate
-    if (count % 2 == 0) 1.0 else -1.0
-  }
-  def sin(sampleRate: Int, index: Int, hertz: Double) =
-    Math.sin((2 * Math.PI * index * hertz) / sampleRate)
-  def generate(sampleRate: Int, hertz: Double, offset: Int, length: Int): Seq[Double] = {
-    Range(0, offset + length)
-      .map(index => {
-        if (index <= offset) 0.0
-        else sin(sampleRate, index, hertz)
-      })
-  }
   class UseCase(implicit val dispatcher: ExecutionContextExecutor) {
-    def use(rollId: Int, input: Input.Get): Future[Output.Get] =
-      for {
-        optRoll <- getRoll(rollId)
-        notes <- getNotesFromRollId(rollId)
-      } yield {
-        val roll = optRoll match {
-          case Some(value) => value
-          case None        => return Future.failed(new Exception(""))
+
+    def use(rollId: Int, input: Input.Get): Future[Output.Get] = {
+      val channel = 1
+      for (map <- RollNoteMap.getFuture(rollId)) yield {
+        val length = input.sampleRate * 4
+        val pcm = map.getPcm(input.sampleRate, 4)
+        Output.Get(channel, length, pcm)
+      }
+    }
+
+    object RollNoteMap {
+      def getFuture(rollId: Int): Future[RollNoteMap] =
+        getRollIdMap(rollId)
+          .map(_.map { case (id, (roll, notes)) => id -> (roll, getFolded(notes)) })
+          .map(it => new RollNoteMap(rollId, it))
+      def getRollIdMap(rollId: Int): Future[Map[Int, (Roll, Seq[Note])]] =
+        getRoll(rollId).flatMap {
+          case None => Future.failed(new Exception(s"rollId: $rollId _ not found on stack"))
+          case Some(roll) =>
+            getNotesFromRollId(rollId)
+              .flatMap { notes =>
+                val futures =
+                  notes.map(_.childRollId match {
+                    case None        => Future { Map.empty }
+                    case Some(value) => getRollIdMap(value)
+                  })
+                Future
+                  .sequence(futures)
+                  .map(_.foldLeft(Map(rollId -> (roll, notes))) { case (sum, it) => sum ++ it })
+              }
         }
+
+      case class Pos(offset: Int, octave: Int, pitch: Int)
+      case class Data(pos: Pos, length: Int, childPianoRoll: Option[Int])
+      case class Fold(store: Seq[Data], current: Data, beforeSticky: Boolean)
+      def getFolded(notes: Seq[Note]): Seq[Data] = {
         val folded =
           notes
             .sortBy(_.offset)
             .sortBy(_.pitch)
             .sortBy(_.octave)
-            .foldLeft(Fold(Seq(), Pos(-1, -1, -1), 0, false))((fold, note) => {
+            .foldLeft(Fold(Seq(), Data(Pos(-1, -1, -1), 0, None), false))((fold, note) => {
+              val current = fold.current
               val samePitch =
-                fold.pos.octave == note.octave &&
-                  fold.pos.pitch == note.pitch
+                current.pos.octave == note.octave &&
+                  current.pos.pitch == note.pitch
               if (samePitch && fold.beforeSticky) {
-                val length = fold.length + 1
-                Fold(fold.store, fold.pos, length, note.sticky)
+                val length = current.length + 1
+                Fold(fold.store, current.copy(length = length), note.sticky)
               } else {
-                val store = fold.store :+ Data(fold.pos, fold.length)
+                val store = fold.store :+ current
                 val pos = Pos(note.offset, note.octave, note.pitch)
-                Fold(store, pos, 1, note.sticky)
+                Fold(store, Data(pos, 1, note.childRollId), note.sticky)
               }
             })
-
-        val maxPitch = 12
-        val keyA = 9
-        val sampleRate = input.sampleRate
-        val bar = 4
-        val foldedNotes = (folded.store :+ Data(folded.pos, folded.length)).tail
-
-        val channel = 1
-        val length = sampleRate * roll.division / 4
-        val beat = length / roll.division
-        println(foldedNotes)
-//        val note = notes.head
-//        val offset = note.offset
-//        val noteLength =
-//          notes
-//            .filter(_.octave == note.octave)
-//            .filter(_.pitch == note.pitch)
-//            .sortBy(_.offset)
-//            .foldLeft(1) { case (sum, it) => if (it.sticky) sum + 1 else sum }
-//        val hertz = getHertz(note.octave * maxPitch + note.pitch - keyA)
-//        val note = foldedNotes.head
-//        val offset = note.pos.offset
-//        val noteLength = note.length
-//        val hertz = getHertz(note.pos.octave * maxPitch + note.pos.pitch - keyA)
-//        val pcm = generate(sampleRate, hertz, offset * beat, noteLength * beat)
-
-        val pcm =
-          foldedNotes
-            .map(note => {
-              val offset = note.pos.offset
-              val noteLength = note.length
-              val hertz = getHertz(note.pos.octave * maxPitch + note.pos.pitch - keyA)
-              generate(sampleRate, hertz, offset * beat, noteLength * beat)
-            })
-            .fold(Seq.empty[Double])((sumPcm, pcm) =>
-              sumPcm
-                .zipAll(pcm, 0.0, 0.0)
-                .map {
-                  case (sum, it) =>
-                    if (sum == 0.0) it
-                    else if (it == 0.0) sum
-                    else sum * 0.5 + it * 0.5
-                }
-            )
-        Output.Get(channel, length, pcm)
+        (folded.store :+ folded.current).tail
       }
+    }
+    class RollNoteMap private (val rootId: Int, val map: Map[Int, (Roll, Seq[RollNoteMap.Data])] = Map.empty) {
+      def getPcm(sampleRate: Int, division: Int): Seq[Double] =
+        getPcm(sampleRate, sampleRate * division, rootId, 0, 0)
+      private def getPcm(sampleRate: Int, length: Int, rollId: Int, divisionSum: Int, offsetSum: Int): Seq[Double] = {
+        val (roll, folded) = map(rollId)
+        val division = roll.division
+        val beat = length / division
+        val maxPitch = 12
+
+        println(s"$rollId: $folded")
+        folded
+          .map(note => {
+            val offset = offsetSum + note.pos.offset * beat
+            val length = note.length * beat
+            val hertz = getHertz(note.pos.octave * maxPitch + note.pos.pitch)
+            println(s"\t$division _ $offset, $length")
+            note.childPianoRoll match {
+              case Some(value) => getPcm(sampleRate, length, value, division, offset)
+              case None        => generate(sampleRate, hertz, offset, length)
+            }
+          })
+          .fold(Seq.empty[Double])((sumPcm, pcm) =>
+            sumPcm
+              .zipAll(pcm, 0.0, 0.0)
+              .map {
+                case (sum, it) =>
+                  if (sum == 0.0) it
+                  else if (it == 0.0) sum
+                  else sum * 0.5 + it * 0.5
+              }
+          )
+      }
+
+      def getHertz(tone: Int): Double = {
+        val keyA = 9
+        val hertzReference = 440.0
+        hertzReference * Math.pow(2, (tone - keyA) / 12.0)
+      }
+      def square(sampleRate: Int, index: Int, hertz: Double): Double = {
+        val count = 2 * index * hertz / sampleRate
+        if (count % 2 == 0) 1.0 else -1.0
+      }
+      def sin(sampleRate: Int, index: Int, hertz: Double) =
+        Math.sin((2 * Math.PI * index * hertz) / sampleRate)
+      def generate(sampleRate: Int, hertz: Double, offset: Int, length: Int): Seq[Double] = {
+        Range(0, offset + length)
+          .map(index => {
+            if (index <= offset) 0.0
+            else sin(sampleRate, index, hertz)
+          })
+      }
+    }
+
   }
+
 }
