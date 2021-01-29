54bda816bf968ebccdb7850caa1b2dc04bb532a8
te170476 <te170476@gmail.com>
2021-01-29 11:28:31 +0900

    update: sound Ignore out-of-range notes

--- src/main/scala/com/github/nnnnusui/musicdsl/usecase/Sound.scala
+++ src/main/scala/com/github/nnnnusui/musicdsl/usecase/Sound.scala
@@ -18,7 +18,7 @@ trait Sound {
       for (map <- RollNoteMap.getFuture(rollId)) yield {
         val length = input.sampleRate * 4
         val pcm = map.getPcm(input.sampleRate, 4)
-        Output.Get(channel, length, pcm)
+        Output.Get(channel, Seq(length, pcm.length).max, pcm)
       }
     }
 
@@ -46,10 +46,16 @@ trait Sound {
     class RollNoteMap private (val rootId: Int, val map: Map[Int, (Roll, Seq[Note])] = Map.empty) {
       def getPcm(sampleRate: Int, division: Int): Seq[Double] =
         getPcm(sampleRate, sampleRate * division, rootId, 0, 0)
-      private def getPcm(sampleRate: Int, length: Int, rollId: Int, offsetSum: Int, relativeTone: Int): Seq[Double] = {
+      private def getPcm(
+          sampleRate: Int,
+          maxLength: Int,
+          rollId: Int,
+          offsetSum: Int,
+          relativeTone: Int
+      ): Seq[Double] = {
         val (roll, folded) = map(rollId)
         val division = roll.division
-        val beat = length / division
+        val beat = maxLength / division
         val maxPitch = 12
 
         folded
@@ -58,10 +64,12 @@ trait Sound {
             val length = note.length * beat
             val tone = relativeTone + (note.octave * maxPitch + note.pitch)
             val hertz = getHertz(tone)
-            note.childRollId match {
-              case Some(value) => getPcm(sampleRate, length, value, offset, tone)
-              case None        => generate(sampleRate, hertz, offset, length)
-            }
+            if (offset >= maxLength) Seq.empty
+            else
+              note.childRollId match {
+                case Some(value) => getPcm(sampleRate, length, value, offset, tone)
+                case None        => generate(sampleRate, hertz, offset, length)
+              }
           })
           .fold(Seq.empty[Double])((sumPcm, pcm) =>
             sumPcm
